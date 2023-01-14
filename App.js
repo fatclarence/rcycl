5fea8201602e881502a6aaa05b571df94c3553d3
anchengyang <anchengyang72@gmail.com>
2023-01-14 22:43:40 +0800

    expo sqlite database

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MapScreen from './screens/MapScreen';
 import ConnectWalletScreen from './screens/ConnectWalletScreen';
 import ScannerScreen from './screens/ScannerScreen';
+import * as SQLite from "expo-sqlite";
 
 const Stack = createNativeStackNavigator();
+const db = SQLite.openDatabase("test4.db");
 
 export default function App() {
+  useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "create table if not exists items (id integer primary key not null, latitude text, longitude text, description text);"
+      );
+      console.log('db created');
+    });
+  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator>
