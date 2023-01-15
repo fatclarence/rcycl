64a61a82616437e16bd80d0d1420bf7f95bc971b
Clarence Koh <ckoh.clarence.96@gmail.com>
2023-01-15 12:14:30 +0800

    side bar accounts

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
+import { LogBox } from "react-native";
 import Main from "./Main";
 import WalletConnectProvider from "react-native-walletconnect";
+
+LogBox.ignoreLogs(["Warning: ..."]); // Ignore log notification by message
+LogBox.ignoreAllLogs(); //Ignore all log notifications
 
 const App = () => {
   return (
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,18 +1,65 @@
-import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Button,
+} from "react-native";
 import React from "react";
 import { useWalletConnect } from "react-native-walletconnect";
-import { withOrientation } from "react-navigation";
+import "@ethersproject/shims";
+// Import the ethers library
+import { ethers } from "ethers";
+
+let gnrToken = require("./abi.json");
 
 const ProfileScreen = () => {
-  const { killSession } = useWalletConnect();
+  const { createSession, killSession, session, signTransaction } =
+    useWalletConnect();
+
+  const fetchBalance = async () => {
+    let tempProvider = new ethers.providers.JsonRpcProvider(
+      "https://sepolia.gateway.tenderly.co/4ueZyXsGXJJ55Yyoyz3JWT"
+    );
+    let tempSigner = tempProvider.getSigner();
+
+    let contract = new ethers.Contract(
+      "0x4db8022Ae2BC5A8fB68Ca848bA6Ae184BbF5b476",
+      gnrToken,
+      tempSigner
+    );
+
+    // const defaultAccount = JSON.stringify(session[0].accounts[0]);
+    const defaultAccount = "0x5601A78F9F2037D996c5a51640628FA9645A28c9";
+
+    let balanceBigN = await contract.balanceOf(defaultAccount);
+    let balanceNumber = balanceBigN.toNumber();
+
+    let decimals = await contract.decimals();
+
+    let tokenBalance = balanceNumber / Math.pow(10, decimals);
+
+    console.log("HELLLOOOOO: ", tokenBalance);
+  };
+
   return (
     <View style={styles.container}>
       {/* <Text>ProfileScreen</Text> */}
-      <TouchableOpacity title="Kill Session" style={styles.button} onPress={() => killSession()}>
+      <TouchableOpacity
+        title="Kill Session"
+        style={styles.button}
+        onPress={() => killSession()}
+      >
         <Text style={styles.buttonText}>Kill Current Session</Text>
       </TouchableOpacity>
 
-      <Image source={require('../icons/icon_transparent.png')} style={styles.image}/>
+      <Image
+        source={require("../icons/icon_transparent.png")}
+        style={styles.image}
+      />
+
+      <Button title="CLICK HERE" onPress={() => fetchBalance()} />
     </View>
   );
 };
@@ -22,24 +69,24 @@ export default ProfileScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'space-between'
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   button: {
     backgroundColor: "#0782F9",
     padding: 20,
-    alignItems: 'center',
-    width: '50%',
+    alignItems: "center",
+    width: "50%",
     borderRadius: 20,
-    marginTop: '50%'
+    marginTop: "50%",
   },
   buttonText: {
-    color: 'white',
+    color: "white",
     fontSize: 20,
   },
   image: {
-    width: '20%',
-    height: '20%',
-    resizeMode: 'contain',
-  }
+    width: "20%",
+    height: "20%",
+    resizeMode: "contain",
+  },
 });
