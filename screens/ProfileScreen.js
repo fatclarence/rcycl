ffc2b89aa9574b9d030c38ec5ad449a63add0afa
rjkoh <koh.ruijie123@gmail.com>
2023-01-15 11:02:33 +0800

    frontend

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,17 +1,45 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import { useWalletConnect } from "react-native-walletconnect";
+import { withOrientation } from "react-navigation";
 
 const ProfileScreen = () => {
   const { killSession } = useWalletConnect();
   return (
-    <View style={{ marginTop: 80 }}>
+    <View style={styles.container}>
       {/* <Text>ProfileScreen</Text> */}
-      <Button title="Kill Session" onPress={() => killSession()} />
+      <TouchableOpacity title="Kill Session" style={styles.button} onPress={() => killSession()}>
+        <Text style={styles.buttonText}>Kill Current Session</Text>
+      </TouchableOpacity>
+
+      <Image source={require('../icons/icon_transparent.png')} style={styles.image}/>
     </View>
   );
 };
 
 export default ProfileScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  button: {
+    backgroundColor: "#0782F9",
+    padding: 20,
+    alignItems: 'center',
+    width: '50%',
+    borderRadius: 20,
+    marginTop: '50%'
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+  },
+  image: {
+    width: '20%',
+    height: '20%',
+    resizeMode: 'contain',
+  }
+});
